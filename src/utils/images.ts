d834c4d8c6d930f16a975e70bc8ba16fb81af344
busraakcay <0busraakcay@gmail.com>
2025-03-31 23:15:46 +0300

    fix: added copy icon

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,4 +2,5 @@ export const images = {
   dice: require("../../assets/icons/dice/dice.png"),
   slash: require("../../assets/icons/slash/slash.png"),
   stars: require("../../assets/icons/stars/stars.png"),
+  copy: require("../../assets/icons/copy/copy.png"),
 };
