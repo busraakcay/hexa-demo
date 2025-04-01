8958e5530d2f5fd8080a7a780bdf65f31da833c7
busraakcay <0busraakcay@gmail.com>
2025-04-02 00:50:23 +0300

    feat: text input created

---
+++ src/components/ui/TextInput/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { View, TextInput as RNTextInput } from "react-native";
+import { styles } from "./styles";
+import { colors } from "../../../theme";
+import { Text } from "../Text";
+import { TextInputProps } from "../../../types";
+
+export const TextInput = ({ text, setText }: TextInputProps): JSX.Element => {
+  const [isFocused, setIsFocused] = useState(false);
+  const maxLength = 500;
+  return (
+    <View
+      style={[
+        styles.inputContainer,
+        { borderColor: isFocused ? "#fff" : "transparent" },
+      ]}
+    >
+      <View style={styles.innerInputContainer}>
+        <RNTextInput
+          style={styles.input}
+          maxLength={maxLength}
+          multiline
+          placeholderTextColor={colors.dark500}
+          placeholder="A blue lion logo reading ‘HEXA’ in bold letters"
+          value={text}
+          returnKeyType="done"
+          blurOnSubmit={true}
+          onChangeText={setText}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+        <View>
+          <Text
+            small
+            extraStyles={styles.maxLength}
+            text={`${text.length}/${maxLength}`}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
---
+++ src/components/ui/TextInput/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../../theme";
+
+export const styles = StyleSheet.create({
+  inputContainer: {
+    borderRadius: 16,
+    borderWidth: 1,
+    width: "100%",
+    backgroundColor: colors.dark2000,
+    overflow: "hidden",
+  },
+  innerInputContainer: {
+    borderRadius: 16,
+    padding: 12,
+    width: "100%",
+    backgroundColor: colors.purple1000RGBA,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.white,
+    textAlignVertical: "top",
+    height: 135,
+  },
+  maxLength: {
+    fontSize: 11,
+    color: colors.dark500,
+  },
+});
